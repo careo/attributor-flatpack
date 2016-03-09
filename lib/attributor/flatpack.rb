46fee796c5d4271a20427b35d33ae30d5c9e0bc6
Dane Jensen <dane.jensen@gmail.com>
2016-03-08 18:05:16 -0800

    fresh off the `bundle gem` presses

---
+++ lib/attributor/flatpack.rb
@@ -0,0 +1,7 @@
+require "attributor/flatpack/version"
+
+module Attributor
+  module Flatpack
+    # Your code goes here...
+  end
+end
---
+++ spec/attributor/flatpack_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Attributor::Flatpack do
+  it 'has a version number' do
+    expect(Attributor::Flatpack::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
