91b87b2c7d8d1e5cac1f8b6a625025853e2a79e3
Dane Jensen <dane.jensen@gmail.com>
2016-03-08 23:05:58 -0800

    add a sample config

---
+++ examples/sample_config.rb
@@ -0,0 +1,44 @@
+require 'pp'
+require 'bundler/setup'
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'attributor/flatpack'
+
+class SampleConfig < Attributor::Flatpack::Config
+  keys do
+    key :rack_env, String
+    key :database do
+      key :host, String
+      key :username, String
+      key :password, String
+    end
+    key :web do
+      key :maxconn, Integer
+      key :workers, Integer
+    end
+    key :world_domination, Attributor::Boolean, default: true
+  end
+end
+
+# a sample ENV
+env = {
+  'RACK_ENV' => 'sekrit-lair',
+  'DATABASE_HOST' => 'postgres://localhost/operation_doomsday',
+  'DATABASE_USERNAME' => 'postgres',
+  'DATABASE_PASSWORD' => '123',
+  'WEB' => JSON.dump(maxconn: 10, workers: 1000)
+}
+
+config = SampleConfig.load(env)
+puts "errors: #{config.validate}"
+# => errors: []
+
+pp config.dump
+# =>
+# {:rack_env=>"sekrit-lair",
+#  :database=>
+#    {:host=>"postgres://localhost/operation_doomsday",
+#     :username=>"postgres",
+#     :password=>"123"},
+#  :web=>{:maxconn=>10, :workers=>1000},
+#  :world_domination=>true}
