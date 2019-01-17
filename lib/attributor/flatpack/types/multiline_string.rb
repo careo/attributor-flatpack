47fc20097597bb610f96935e0265617d7a8a6231
Mark Dotson <mastamark+github@gmail.com>
2019-01-17 12:20:38 -0800

    added new type MultilineString

---
+++ lib/attributor/flatpack/types/multiline_string.rb
@@ -0,0 +1,20 @@
+# Sometimes a multline string when read in by ruby has escaped newlines, sometimes not.
+# This type will remove any escaping of newline from a string.
+
+module Attributor
+  module Flatpack
+    class MultilineString < Attributor::String
+
+      def self.load(value, context = Attributor::DEFAULT_ROOT_CONTEXT, **options)
+        value.gsub('\\n', "\n")
+      rescue StandardError
+        super
+      end
+
+      def self.example(_context = nil, options: {})
+        "-----BEGIN EC PRIVATE KEY-----\\nMIHcAgEBBEI\\abcdefghijklmnop==\\n-----END EC PRIVATE KEY-----"
+      end
+
+    end
+  end
+end
